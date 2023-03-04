65a37006c23856020c697d5d97bbdff6fcd42145
ninlilizi <nin@nkli.net>
2023-03-04 05:39:04 +0000

    I forgot to update the headers, doh.

--- OptixDenoiser.h
+++ OptixDenoiser.h
@@ -31,14 +31,10 @@ OPTIXDENOISER_API cudaError_t __cdecl createCudaStream(cudaStream_t* stream);
 OPTIXDENOISER_API cudaError_t __cdecl destroyCudaStream(cudaStream_t stream);
 
 // Creates a denoiser object with the given options.
-OPTIXDENOISER_API OptixResult __cdecl createDenoiser(OptixDeviceContext context, const OptixDenoiserOptions* options, OptixDenoiser* denoiser);
+OPTIXDENOISER_API OptixResult __cdecl createDenoiser(OptixDeviceContext context, OptixDenoiserModelKind kind, const OptixDenoiserOptions* options, OptixDenoiser* denoiser);
 
 // Destroys the denoiser object and any associated host resources.
 OPTIXDENOISER_API OptixResult __cdecl destroyDenoiser(OptixDenoiser denoiser);
-
-// Sets the model of the denoiser.
-// If the kind is OPTIX_DENOISER_MODEL_KIND_USER, then the data and sizeInByes must not be nulland zero respectively.For other kinds, these parameters must be zero.
-OPTIXDENOISER_API OptixResult __cdecl setDenoiserModel(OptixDenoiser denoiser, OptixDenoiserModelKind kind, void* data, size_t sizeInBytes);
 
 OPTIXDENOISER_API OptixResult __cdecl computeIntensity(OptixDenoiser denoiser, CUstream stream, const OptixImage2D* inputImage, CUdeviceptr outputIntensity, 
 	CUdeviceptr scratch, size_t scratchSizeInBytes);
@@ -50,7 +46,7 @@ OPTIXDENOISER_API OptixResult __cdecl setupDenoiser(OptixDenoiser denoiser, CUst
 // Invokes denoiser on a set of input dataand produces one output image.Scratch memory must be available during the execution of the denoiser.
 OPTIXDENOISER_API OptixResult __cdecl invokeDenoiser(
 	OptixDenoiser denoiser, CUstream stream, const OptixDenoiserParams* params, CUdeviceptr denoiserState, size_t denoiserStateSizeInBytes,
-	const OptixImage2D* inputLayers, unsigned int numInputLayers, unsigned int inputOffsetX, unsigned int inputOffsetY, const OptixImage2D* outputLayer,
+	const  OptixDenoiserGuideLayer* guideLayer, const OptixDenoiserLayer* inputLayers, unsigned int numInputLayers, unsigned int inputOffsetX, unsigned int inputOffsetY, const OptixImage2D* outputLayer,
 	CUdeviceptr scratch, size_t scratchSizeInBytes);
 
 // Computes the GPU memory resources required to execute the denoiser.
